c2f9efa470bef04d1ce316a1a800c078db4a2fcc
Zahra <hizahrazadeh@gmail.com>
2021-02-14 18:46:58 -0500

    updated total mem

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
-const path = require('path');
 
-var pathObj = path.parse(__filename);
+const os = require('os');
+var totalMem = os.totalmem();
+var freeMem = os.freemem();
 
-console.log(pathObj);
+console.log(`Total Memory: ${totalMem}`);
+console.log(`Free Memory: ${freeMem}`);
