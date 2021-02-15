5a699255be2f87d2ef846c947dfca464a01c4ac7
Zahra <hizahrazadeh@gmail.com>
2021-02-14 20:19:20 -0500

    event listener  enter the commit message for your changes. Lines starting

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
-const fs = require('fs');
 
-// const files = fs.readdirSync('./');
 
-fs.readdir('$', function(err, files) {
-    if (err) console.log('Error', err);
-    else console.log('Result', files);
+const EventEmitter = require('events');
+//convention to camel case to show that EventEmitter is a CLASS
+//a class is a container for properties and functions which we call functions (related methods and properties)
+
+const emitter = new EventEmitter();
+//Register a listener [a listener is a function that will be called
+//when that event is raised]
+emitter.on('messageLogged', function(){
+    console.log('Listener called')
 })
+//Raise an event (below)
+emitter.emit('messageLogged')
+
