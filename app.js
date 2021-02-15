ff929e4744620c94c4badada176a9e21d3e7039b
Zahra <hizahrazadeh@gmail.com>
2021-02-14 20:41:43 -0500

    Changed eventEmitter to logger

--- app.js
+++ app.js
@@ -4,15 +4,21 @@ const EventEmitter = require('events');
 //convention to camel case to show that EventEmitter is a CLASS
 //a class is a container for properties and functions which we call functions (related methods and properties)
 
-const emitter = new EventEmitter();
 //Register a listener [a listener is a function that will be called
 //when that event is raised]
-emitter.on('messageLogged', (eventArg) => {
+
+const Logger = require('./logger');
+const logger = new Logger();
+
+
+logger.on('messageLogged', (eventArg) => {
     console.log('Listener called', eventArg)
 })
 
-const log = require('./logger');
-log('message');
+logger.log('message');
+
+
+
 
 //Raise an event called logging (data: message)
 
--- logger.js
+++ logger.js
@@ -1,15 +1,19 @@
 
 var url = 'http://mylogger.io/log';
 const EventEmitter = require('events');
-const emitter = new EventEmitter();
 
-function log(message) {
-    //Send an http request
-    console.log(message);
-//Raise an event (below)
-    emitter.emit('messageLogged', { id: 1, url: 'http://' })
-
+//by using extends EventEmitter, this class Logger
+//now has all the functionality that is defined in EventEmitter
+class Logger extends EventEmitter {
+    log(message) {
+        //Send an http request
+       console.log(message);
+    //Raise an event (below)
+       this.emit('messageLogged', { id: 1, url: 'http://' })
+    
+    }
 }
 
-module.exports = log; //you can export a function or a single object
+
+module.exports = Logger; //you can export a function or a single object
 
